5630f7236b8e3679b766d048ab932739d0475028
Mahendran <mahendran.d4897@gmail.com>
2020-03-17 00:55:56 +0530

    module-1 completed

---
+++ src/app.js
@@ -0,0 +1,17 @@
+let fs= require('fs');
+let path= require('path');
+let express= require('express');
+
+let app=express();
+app.set('views',path.join(__dirname,'/views'))
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/',(request,response)=>{
+    response.render('index',{title:'Index'});
+})
+
+app.listen(3000,()=>{});
+
+console.log("Server created with 3000!");
