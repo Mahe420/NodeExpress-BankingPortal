7865ba847b5883e29556c006f43852f5dfb9a1e1
Mahendran <mahendran.d4897@gmail.com>
2020-03-17 20:20:33 +0530

    Completed module

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views',path.join(__dirname,'/views'))
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
 
 const accountData=fs.readFileSync("src/json/accounts.json","utf8");
 const accounts=JSON.parse(accountData);
@@ -21,18 +22,53 @@ app.get('/',(request,response)=>{
     accounts: accounts});
 })
 
-app.get('/savings',(resquest,response)=>{
+app.get('/savings',(request,response)=>{
     response.render('account',{account:accounts.savings});
 })
-app.get('/checking',(resquest,response)=>{
+app.get('/checking',(request,response)=>{
     response.render('account',{account:accounts.checking});
 })
-app.get('/credit',(resquest,response)=>{
+app.get('/credit',(request,response)=>{
     response.render('account',{account:accounts.credit});
+})
+
+app.get('/transfer',(request,response)=>{
+    response.render('transfer');
 })
 
 app.get('/profile',(request,response)=>{
     response.render('profile',{user:users[0]});
+})
+
+app.post('/transfer',(request,response)=>{
+    var from=request.body.from;
+    var to=request.body.to;
+    var amount=request.body.amount;
+
+    accounts[from].balance-=amount;
+    accounts[to].balance+=parseInt(amount,10);
+
+    let accountsJSON=JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+
+    response.render('transfer',{message: "Transfer Completed"});
+});
+
+app.get('/payment',(request,response)=>{
+    response.render('payment',{account:accounts.credit});
+})
+app.post('/payment',(request,response)=>{
+    
+    var amount=request.body.amount;
+
+    accounts.credit.balance-=amount;
+    accounts.credit.available=parseInt(accounts.credit.available)+parseInt(amount);
+
+    let accountsJSON=JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname,'json','accounts.json'),accountsJSON,'utf8');
+
+response.render('payment',{ message: "Payment Successful", account: accounts.credit });
+
 })
 
 app.listen(3000,()=>{ console.log('PS Project Running on port 3000!') });
